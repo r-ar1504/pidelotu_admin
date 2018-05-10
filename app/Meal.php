866306618ac7bad7cca732ac683f7893cb26e7c0
rRios15041995 <rodolfo.r1504@gmail.com>
2018-05-10 17:46:33 -0500

    Meals

--- app/Meal.php
+++ app/Meal.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meal extends Model
 {
-  protected $table = 'meals';
-  public function restaurant(){
-    return $this->belongsTo('App\MealCategory');
-  }
+   / *Meal Belongs To Category*/
+   public function category(){
+     return $this->BelongsTo('App\MealCategory');
+   }
+   /*Has Many Ingredients*/
+   public function ingredients(){
+     return $this->hasMany('App\Ingredient');
+   }
 }
--- app/MealCategory.php
+++ app/MealCategory.php
@@ -6,14 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MealCategory extends Model
 {
-  protected $fillable = array('name', 'dashboard_banner', 'image', 'restaurant_id');
-  protected $table = 'meal_categories';
-  /*Category Belongs To Restaurant*/
-  public function restaurant(){
-    return $this->belongsTo('App\Restaurant');
-  }
-  /*Has Many Meals*/
-  public function meals(){
-    return $this->hasMany('App\Meal');
-  }
+   /*Category Belongs To Restaurant*/
+   public function restaurant(){
+     return $this->belongsTo('App\Restaurant');
+   }
+   /*Has Many Meals*/
+   public function meals(){
+     return $this->hasMany('App\Meal','category_id');
+   }
 }
